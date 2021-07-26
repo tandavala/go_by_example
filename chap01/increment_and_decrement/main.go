c9e22c398e967ac27f315e331402b60651148792
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2021-07-26 20:13:47 +0100

    increment and decrement

---
+++ chap01/increment_and_decrement/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+// declare variables
+var a = 4
+
+// increment
+fmt.Printf("a = %d \n", a)
+a = a + 1
+fmt.Printf("a + 1 = %d \n", a)
+a++
+fmt.Printf("a++ = %d \n", a)
+
+// decrement
+a = a - 1
+fmt.Printf("a - 1 = %d \n", a)
+a--
+fmt.Printf("a-- =%d \n", a)
+}
