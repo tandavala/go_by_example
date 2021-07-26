ea48376f24755ef41a7c0810c092ebc389c17e80
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2021-07-26 20:09:18 +0100

    mathimatical functions

--- chap01/arithmetic_operations/main.go
+++ chap01/arithmetic_operations/main.go
@@ -13,18 +13,18 @@ b = 10
 // arithmetic operation
 // additon
 c := a + b
-fmt.Println("%d + %d = %d \n", a, b, c)
+fmt.Printf("%d + %d = %d \n", a, b, c)
 
 // Subtraction
 d := a - b
-fmt.Println("%d - %d = %d \n", a, b, d)
+fmt.Printf("%d - %d = %d \n", a, b, d)
 
 // division
 e :=  float32(a) / float32(b)
-fmt.Println("%d / %d = %d \n", a, b, e)
+fmt.Printf("%d / %d = %d \n", a, b, e)
 
 // multiplication
 f := a * b
-fmt.Println("%d * %d = %d \n", a, b, f)
+fmt.Printf("%d * %d = %d \n", a, b, f)
 
 }
