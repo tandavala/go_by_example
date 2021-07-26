3be64618d9f1f8a501d5e3734cdae6f5728617a2
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2021-07-26 20:21:13 +0100

    Comparison Operators

---
+++ chap01/comparison_operators/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var (
+		a = 2
+		b = 10
+ )
+
+	fmt.Println(a>b)
+	fmt.Println(a<b)
+	fmt.Println(a>=b)
+	fmt.Println(a<=b)
+	fmt.Println(a!=b)
+	fmt.Println(a==b)
+}
