bbac248dadfa4ff9f06bb3b95c9f3af7a6618239
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2021-07-26 19:04:45 +0100

    declaring variables

---
+++ chap01/declaragin_variables/main.go
@@ -0,0 +1,27 @@
+var str string
+var n, m int
+var  mn float32
+
+// We can also declare multiple variables
+
+var (
+	name string
+	email string
+	age int
+)
+
+// Assigning Variables
+str = "Hello World"
+n = 10
+m = 50
+mn = 2.45
+
+// We also declare a variable and assign its value
+var city  string = "Lobito"
+var x int = 100
+
+// OR
+country := "Angola"
+val := 15
+
+
