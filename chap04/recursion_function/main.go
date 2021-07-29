b50325518c8135103a9412c9fced1bfcf3084c85
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2021-07-29 05:14:48 +0100

    recursin function

---
+++ chap04/recursion_function/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func fibonacci(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	}
+	return (fibonacci(n-1) + fibonacci(n-2))
+}
+
+func main(){
+	fmt.Println(fibonacci(20))
+}
