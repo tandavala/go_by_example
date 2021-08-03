52873f955753ef24c99df617649e19cf4f49b0d3
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2021-08-03 05:33:32 +0100

    check string data length

---
+++ chap07/check_string_data_length/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("------- demo String Length-------")
+	str_data := "welcome to go"
+	len_data := len(str_data)
+	fmt.Printf("len=%d \n", len_data)
+}
