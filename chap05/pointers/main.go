ba5f283ace7e9a498d3524cc03583cd1ff03ec70
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2021-08-03 04:21:06 +0100

    working with pointer in golang

--- chap05/pointers/main.go
+++ chap05/pointers/main.go
@@ -8,4 +8,21 @@ func main(){
 	x = 10
 	fmt.Println(x)
 	fmt.Println(&x)
+
+	// declare pointer
+	var num *int
+	val := new(int)
+
+	num = new(int)
+	*num = x // set value
+
+	val = &x // set address
+
+	fmt.Println("========= pointer num ============")
+	fmt.Println(*num) // print a value of x
+	fmt.Println(num) // print address
+	fmt.Println("================  pointer val =======")
+	fmt.Println(*val)
+	fmt.Println(val) // print address of x
+
 }
