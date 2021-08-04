0fc01f54a4bda27b284f2e76f75251dc06e1758e
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2021-08-04 04:47:16 +0100

    formatting logging

--- chap09/Logging/main.go
+++ chap09/Logging/main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"fmt"
 	"log"
-	//"os"
+	"os"
 )
 
 func main(){
-	simpleLogging()
+	//simpleLogging()
+	formattingLogging()
 }
 
 func simpleLogging(){
 	fmt.Println("------ simple logging ------")
 	log.Println("Hello World")
 	log.Println("this is a simple error")
+}
+
+func formattingLogging(){
+	fmt.Println("--------- formattingLogging---------")
+	var warning *log.Logger
+
+	warning = log.New(
+		os.Stdout,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+	warning.Println("This is warning message 1")
+	warning.Println("This warning message 2")
 }
